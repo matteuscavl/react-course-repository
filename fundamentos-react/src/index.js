1203f7a15b0002a3a9a3b842f7120a29cfeb598b
matteuscavl <mateuslcavl@gmail.com>
2020-10-02 23:23:02 -0300

    Aula Components

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,4 +1,17 @@
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
 
-const el = document.getElementById('root')
-ReactDOM.render('Olá React!', el)
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+
+ReactDOM.render(
+    <div>
+        {/* <Primeiro></Primeiro> */}
+        <ComParametro titulo="Situação Aluno"
+        aluno="Pedro" nota={5}></ComParametro>
+
+        <ComParametro titulo="Situação Aluna" aluno='Maria' nota={10}></ComParametro>
+    </div>, 
+    document.getElementById('root')
+)

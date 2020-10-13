41a81ce31c55a12975224d280bd1447777054d04
matteuscavl <mateuslcavl@gmail.com>
2020-10-13 15:10:47 -0300

    Organize your money - projeto de fixacacao e estudo

---
+++ Exercicios de fixacao/organize your money/index.js
@@ -0,0 +1,30 @@
+// Instanciando Express para Rotas
+// Instanciando o Body Parser para tratar requisições advindas do Front-End
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Iniciando o App
+// Definindo a porta da aplicação
+const app = express();
+const port = 8181;
+
+// Configurando o EJS 
+app.set('view engine', 'ejs');
+
+// Configurando os arquivos estáticos
+app.use(express.static('public'));
+
+// Configurando o BodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas
+app.get('/', (req, res) => {
+    res.render('inicio');
+})
+
+
+// Upando o servidor
+app.listen(port, _ => {
+    console.log('Servidor rodando');
+})

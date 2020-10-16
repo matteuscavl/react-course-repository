e80128b260008f50fcec6c5d4765eead8e134d8c
matteuscavl <mateuslcavl@gmail.com>
2020-10-15 22:42:59 -0300

    Iniciada a modelagem de Artigos

--- Projetos Praticos/Blog com painel administrativo - CRUD/articles/articlesController.js
+++ Projetos Praticos/Blog com painel administrativo - CRUD/articles/articlesController.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
 // Rotas de Artigos
-router.get('/articles', (req, res) => {
-    res.send('Novo artigo');
+router.get('/admin/articles', (req, res) => {
+    res.render('admin/articles/index')
 })
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('Novo artigo admin');
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories});
+    })
+})
+
+router.post('/articles/save', (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title: title, 
+        slug: slugify(title), 
+        body: body, 
+        categoryId: category,
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
 })
 
 module.exports = router;
